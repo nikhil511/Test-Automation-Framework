c79f017977c17817db8c7aa0a2a9f958c50be202
Nikhil Narkedamilli <narkedamillinikhil@gmail.com>
2024-12-08 22:42:41 +0530

    updated test data utility files

--- src/test/java/com/utility/PropertiesUtil.java
+++ src/test/java/com/utility/PropertiesUtil.java
@@ -12,7 +12,7 @@ public class PropertiesUtil {
 
 	public static String readProperty(Env env, String propertyName) {
 
-		File propfile = new File(System.getProperty("user.dir") + "\\config\\" + env + ".properties");
+		File propfile = new File(System.getProperty("user.dir") + "/config/" + env + ".properties");
 		FileReader fr;
 		Properties properties = new Properties();
 		try {
